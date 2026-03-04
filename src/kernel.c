a9242d305b11c8b250213a2d1354f7a9b21733a5
Samhith Enganti <samhithenganti@gmail.com>
2026-03-04 14:23:31 -0500

    added print_hex functions

--- src/kernel.c
+++ src/kernel.c
@@ -4,14 +4,41 @@
 #define VGA_ADDRESS 0xB8000
 #define WHITE_ON_BLACK 0x0F
 
-void print_string(const char *str) {
+static int cursor_offset = 0;
+
+void print_char(char c) {
   char *video_memory = (char *)VGA_ADDRESS;
-  int offset = 0;
+  video_memory[cursor_offset] = c;
+  video_memory[cursor_offset + 1] = WHITE_ON_BLACK;
+  cursor_offset += 2;
+}
+
+void print_string(const char *str) {
   while (*str != '\0') {
-    video_memory[offset] = *str;
-    video_memory[offset + 1] = WHITE_ON_BLACK;
-    str++;
-    offset += 2;
+    print_char(*str++);
+  }
+}
+
+void print_hex_byte(uint8_t byte) {
+  char *hex_digits = "0123456789ABCDEF";
+
+  print_string("0x");
+
+  uint8_t high = (byte >> 4) & 0x0F;
+  uint8_t low = byte & 0x0F;
+
+  print_char(hex_digits[high]);
+  print_char(hex_digits[low]);
+}
+
+void print_hex(uint32_t value) {
+  char *hex_digits = "0123456789ABCDEF";
+
+  print_string("0x");
+
+  for (int i = 28; i >= 0; i -= 4) {
+    uint8_t nibble = (value >> i) & 0x0F;
+    print_char(hex_digits[nibble]);
   }
 }
 
@@ -23,4 +50,7 @@ void kernel_main() {
   if (buffer[510] == 0x55 && buffer[511] == 0xAA) {
     print_string("Read disk!");
   }
+
+  print_hex_byte(buffer[510] == 0x55);
+  print_hex_byte(buffer[511] == 0x55);
 }
