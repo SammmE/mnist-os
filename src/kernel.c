3ed8b3289083e639b0e8ecf9f0b3f18fdf907712
Samhith Enganti <enganti.samhith@gmail.com>
2026-03-05 23:28:35 -0500

    load WEIGHTS.BIN into W1, b1, W2, b2

--- src/kernel.c
+++ src/kernel.c
@@ -1,8 +1,14 @@
-#include "disk.h"
 #include "fat32.h"
 #include "math.h"
 #include "types.h"
 #include "vga.h"
+
+float network_weights[7510];
+
+float *W1;
+float *b1;
+float *W2;
+float *b2;
 
 void kernel_main() {
   clear_screen();
@@ -13,14 +19,23 @@ void kernel_main() {
 
   struct DirectoryEntry test_file;
 
-  if (fat32_find_file("HELLO   TXT", &test_file)) {
-    print_string("Found file! Size: ");
+  if (fat32_find_file("WEIGHTS BIN", &test_file)) {
+    print_string("Found Weights! Size: ");
     println_hex(test_file.size);
 
-    uint8_t file_data[8192];
-    fat32_read_file(&test_file, file_data);
+    // W_1 = 64 * 100 = 6400 floats
+    // b_1 =            100 floats
+    // W_2 = 100 * 10 = 1000 floats
+    // W_1 =            10 floats
+    // TOTAL =          30040 bytes
+    uint8_t file_data[30040];
 
-    println_string((const char *)file_data);
+    fat32_read_file(&test_file, (uint8_t *)network_weights);
+
+    W1 = network_weights;
+    b1 = W1 + (64 * 100);
+    W2 = b1 + 100;
+    b2 = W2 + (100 * 10);
   }
 
   float a = 6.7f;
